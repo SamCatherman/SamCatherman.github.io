8b6e7d649ffa80015f78502f63641ba868af2d93
SamCatherman <sam.catherman@gmail.com>
2017-08-29 19:15:01 -0400

    adds new projects, restyles

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ var button = document.getElementById('button');
 
 button.onclick = function () {
   var div = document.getElementById('hideMe');
+  var button = document.getElementById('button');
+
   if (div.style.display !== 'none') {
     div.style.display = 'none';
+    button.innerHTML = 'Show';
   }
   else {
     div.style.display = 'block'
+    button.innerHTML = 'Hide';
   }
 }
